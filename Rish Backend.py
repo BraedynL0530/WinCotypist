3f92b0df6c8055c24b2bf7eae0b8e1b90da0a660
Rishaan Bhattacharya <rishaanb65@gmail.com>
2026-09-18 05:21:03 +0530

    testing git not final code almost done dont worry

--- Rish Backend.py
+++ Rish Backend.py
@@ -1,1 +1,13 @@
-print()
+import json
+json_string= '''
+{"students":[{"name":"Rishaan","Age":45,"Grade":10},{"gender":"male","REligion":"Atheist"}]}
+'''
+
+data=json.loads(json_string)
+print(data)
+def weather_now(x):
+    if x>=20:
+        print("It is cold today")
+    else:
+         print("It is hot today")
+
