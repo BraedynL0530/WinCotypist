3ef0a1c5d1954357b8751572fb2bf005769415b4
Rishaan Bhattacharya <rishaanb65@gmail.com>
2026-09-18 23:20:33 +0530

    Main parser done, now testing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import json
 from typing import Any
+import requests
 from urllib import response
 
 
@@ -13,11 +14,11 @@ def parse_ai_repsonse(json_data:str) -> tuple[str,float]:
     Args:
         json_data (str): the AI's JSON repsonse
     Returns:
-          A tuple contianing the response and confidence
-          - repsonse: The ai's response as a string
+          A tuple containing the response and confidence
+          - respsonse: The ai's response as a string
           -confidence: The Ai's confidence as a float.
     Raises:
-          TypeErro: If response is not a string
+          TypeError: If response is not a string
           ValueError: If the JSON is invalid
           KeyError: If response of confidence is missing
       """
@@ -26,7 +27,23 @@ def parse_ai_repsonse(json_data:str) -> tuple[str,float]:
     except json.JSONDecodeError as error:
         raise ValueError("Invalid JSON response") from error
     response = data["response"]
-    confidenc = float(data["confidence"])
+    confidence = float(data["confidence"])
+
+    if not isinstance(response, str):
+        raise TypeError("response must be a string")
+    return response,confidence
+
+def send_text_to_server(text: str, url: str) -> requests.Response:
+    """
+    Send test to a server using an HTTP POST request
+    Args:
+         text: Text to send to the server.
+         url: URL of the server.
+     Returns:
+         THe HTTP response from the server
+
+    """
+    return requests.post(url, json={"text": text},timeout=10,)
 
 
 
