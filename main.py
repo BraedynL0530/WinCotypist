cc346794d97db79b8a623e80ba2982c8969e3cb5
Rishaan Bhattacharya <rishaanb65@gmail.com>
2026-09-18 23:13:05 +0530

    Almost there

--- main.py
+++ main.py
@@ -1,13 +1,37 @@
 import json
-json_string= '''
-{"students":[{"name":"Rishaan","Age":45,"Grade":10},{"gender":"male","REligion":"Atheist"}]}
-'''
+from typing import Any
+from urllib import response
 
-data=json.loads(json_string)
-print(data)
-def weather_now(x):
-    if x>=20:
-        return "hot"
-    else:
-        return "cold"
+
+def parse_ai_repsonse(json_data:str) -> tuple[str,float]:
+    """
+    Parse an AI JSON repsonse.
+    The JSON is expected to contain:
+    - response: a string containing the AI's Response
+    - cpmfidence : a number representing the AI's confidence
+
+    Args:
+        json_data (str): the AI's JSON repsonse
+    Returns:
+          A tuple contianing the response and confidence
+          - repsonse: The ai's response as a string
+          -confidence: The Ai's confidence as a float.
+    Raises:
+          TypeErro: If response is not a string
+          ValueError: If the JSON is invalid
+          KeyError: If response of confidence is missing
+      """
+    try:
+        data: dict[str,Any] = json.loads(json_data)
+    except json.JSONDecodeError as error:
+        raise ValueError("Invalid JSON response") from error
+    response = data["response"]
+    confidenc = float(data["confidence"])
+
+
+
+
+
+
+
 
--- test.py
+++ test.py
@@ -1,4 +1,2 @@
-from __main__ import weather_now
-def test_weather_now():
-    assert weather_now(21) == "hot"
+import
 
